71e2cbea9d567d17fc1fc22f50e2c363c44d1c1b
Alexa-2k <109705749+Alexa-2k@users.noreply.github.com>
2024-07-27 18:23:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const btnCopy = document.querySelector("#copy_btn");
 
 
 // Configuración inicial de la imagen de fondo en outMessage
-outMessage.style.backgroundImage = "url('./Images/lock.png')";
+outMessage.style.backgroundImage = "url('../Images/lock.png')";
 
 inputVerificar();
 
@@ -18,6 +18,7 @@ btnEncrypt.onclick = (e) => {
     outMessage.value = encodedTxt;
     input.value = "";
     outMessage.style.backgroundImage = "none"; // Quitar la imagen de fondo al codificar
+    outMessage.classList.add("focused");
 }
 
 // Captura la id en el momento del click y direcciona el programa para el método que DESENCRIPTA el texto
@@ -26,7 +27,8 @@ btnDecrypt.onclick = (e) => {
     const decodedTxt = decrypt(input.value);
     outMessage.value = decodedTxt;
     input.value = "";
-    outMessage.style.backgroundImage = "none"; // Quitar la imagen de fondo al decodificar
+    outMessage.style.backgroundImage = "none"; // QuitaR la imagen de fondo al decodificar
+    outMessage.classList.add("focused");
 }
 
 // Captura la id en el momento del click y hace la validación que copia el texto
@@ -38,7 +40,9 @@ btnCopy.onclick = (e) => {
         // Una vez copiado, mueve el texto a txt_input
         input.value = outMessage.value;
         outMessage.value = "";
-        outMessage.style.backgroundImage = "url('./Images/lock.png')"; // Restablecer la imagen de fondo
+        outMessage.style.backgroundImage = "url('../Images/lock.png')"; // Restablecer la imagen de fondo
+        outMessage.classList.remove("focused");
+   
     }).catch(err => {
         console.error('Error al copiar el texto: ', err);
     });
@@ -63,7 +67,7 @@ function decrypt(decodedString) {
     return decodedString;
 }
 
-// Verifica lo ingresado por el usuario
+// Verifica lo ingresado por el usuario y bloquea los caracteres que no sean letras
 function inputVerificar() {
     input.addEventListener("keydown", function (e) {
         const keyCode = (e.keyCode ? e.keyCode : e.which);
